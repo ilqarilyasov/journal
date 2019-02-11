3ac51acae779bc73fab5e06359293c59c799ae92
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-11 15:09:13 -0600

    Implement EntryDetailViewController's updateViews() and saveBarButtonTapped(_ sender:) functions

--- FournalCoreData/View Controllers/EntryDetailViewController.swift
+++ FournalCoreData/View Controllers/EntryDetailViewController.swift
@@ -13,7 +13,9 @@ class EntryDetailViewController: UIViewController {
     // MARK: - Properties
     
     var entryController: EntryController?
-    var entry: Entry?
+    var entry: Entry? {
+        didSet { updateViews() }
+    }
     
     
     // MARK: - Outlets
@@ -26,7 +28,39 @@ class EntryDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateViews()
     }
+    
+    
+    // MARK: - Update views
+    
+    private func updateViews() {
+        guard isViewLoaded else { return }
+        
+        if let entry = entry {
+            title = entry.title
+            titleTextField.text = entry.title
+            bodyTextTextView.text = entry.bodyText
+        } else {
+            title = "Create Entry"
+        }
+    }
+    
 
+    // MARK: - Actions
+    
+    @IBAction func saveBarButtonTapped(_ sender: Any) {
+        guard let title = titleTextField.text,
+            !title.isEmpty,
+            let bodyText = bodyTextTextView.text else { return }
+        
+        if let entry = entry {
+            entryController?.update(entry: entry, title: title, bodyText: bodyText)
+        } else {
+            entryController?.createEntry(with: title, bodyText: bodyText)
+        }
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
