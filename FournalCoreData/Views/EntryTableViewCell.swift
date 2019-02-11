ddc46d66361b305b11f9573a1979d08340bf35dd
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-11 13:38:01 -0600

    Lay out EntriesTableViewController and its custom cell. Add outlets

---
+++ FournalCoreData/Views/EntryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EntryTableViewCell.swift
+//  FournalCoreData
+//
+//  Created by Ilgar Ilyasov on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleTextLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyTextLabel: UILabel!
+    
+
+}
