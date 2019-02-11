a5927073dc65a00d47cf1fad76675bccba1c7ce4
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-11 14:42:14 -0600

    Implement EntryTableViewCell's updateViews function

--- FournalCoreData/Views/EntryTableViewCell.swift
+++ FournalCoreData/Views/EntryTableViewCell.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class EntryTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    var entry: Entry? {
+        didSet { updateViews() }
+    }
+    
+    
+    // MARK: - Helper
+    
+    lazy var formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    // MARK: - Outlets
 
     @IBOutlet weak var titleTextLabel: UILabel!
     @IBOutlet weak var timestampLabel: UILabel!
     @IBOutlet weak var bodyTextLabel: UILabel!
     
+    
+    // MARK: - Update views
+    
+    private func updateViews() {
+        guard let entry = entry,
+            let date = entry.timestamp else { return }
+        
+        titleTextLabel.text = entry.title
+        bodyTextLabel.text = entry.bodyText
+        timestampLabel.text = formatter.string(from: date)
+    }
 
 }
