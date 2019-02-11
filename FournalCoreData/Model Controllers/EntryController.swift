9462877d6998ddec39f46c480c2ecec5e712cd91
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-11 14:28:05 -0600

    Implement save to and load from PersistentStore function

---
+++ FournalCoreData/Model Controllers/EntryController.swift
@@ -0,0 +1,37 @@
+//
+//  EntryController.swift
+//  FournalCoreData
+//
+//  Created by Ilgar Ilyasov on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class EntryController {
+    
+    // MARK: - Persistent Coordinator
+    
+    func saveToPersistentStore() {
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() -> [Entry] {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        let moc = CoreDataStack.shared.mainContext
+        
+        do {
+            let entries = try moc.fetch(fetchRequest)
+            return entries
+        } catch {
+            NSLog("Error fetching from managed object context: \(error)")
+            return []
+        }
+    }
+    
+}
