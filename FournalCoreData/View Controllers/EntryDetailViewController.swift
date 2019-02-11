c1a6eb13ec71cb3539f6b3d34f1dafbadab06501
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-11 13:42:11 -0600

    Lay out EntryDetailViewController scene in storyboard and add outlet connetions

---
+++ FournalCoreData/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EntryDetailViewController.swift
+//  FournalCoreData
+//
+//  Created by Ilgar Ilyasov on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextTextView: UITextView!
+    
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
